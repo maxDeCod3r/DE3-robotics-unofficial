e9fbdffa7888da58a2c5c46fcec7298a0964b37a
Max Hunt <mh2817@ic.ac.uk>
2020-02-10 20:33:15 +0000

    Update

--- listener.py
+++ listener.py
@@ -11,9 +11,10 @@ if __name__ == '__main__':
 	while not rospy.is_shutdown():
 		try:
 			(trans,rot) = listener.lookupTransform('left_gripper', 'base', rospy.Time(0))
+			print((trans, rot))
 		# except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 		except: continue
-		
+
 		Translation = [trans.x , trans.y , trans.z]
 		Quaternion = [rot.x , rot.y , rot.z, rot.w]
 		Angles = tf.transformations.euler_from_quaternion([rot.x , rot.y ,
