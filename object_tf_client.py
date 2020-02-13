86b7db57670973f88446e33867a67c1cc9f578b9
Max Hunt <mh2817@ic.ac.uk>
2020-02-13 14:40:34 +0000

    Update

--- object_tf_client.py
+++ object_tf_client.py
@@ -18,7 +18,7 @@ def tf_lookup(object_name):
 			time.sleep(0.1)
 			continue
 
-		object_angles = tf.transformations.euler_from_quaternion([rot[3] , rot[0], rot[1], rot[2]], axes='sxyz')
+		object_angles = tf.transformations.euler_from_quaternion([rot[0] , rot[1], rot[2], rot[3]])
 		xangle = object_angles[0]# + radians(196)
 		xangle1 = object_angles[1]# + radians(90)
 		xangle2 = object_angles[2]# + radians(-106)
