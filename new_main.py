28f206a1619c0f6dec6ebf0cb205b21bd4522d2d
Max Hunt <mh2817@ic.ac.uk>
2020-02-15 14:43:15 +0000

    Update

--- new_main.py
+++ new_main.py
@@ -22,7 +22,7 @@ brickstuff = tps.brick_directions_notf
 
 class PickAndPlace(object):
     def __init__(self, limb, hover_distance = 0.10, verbose=True, speed=0.2, accuracy=baxter_interface.settings.JOINT_ANGLE_TOLERANCE):
-        self._speed = speed
+        self._speed = 0.2
         self._accuracy = accuracy
         self._limb_name = limb # string
         self._hover_distance = hover_distance # in meters
